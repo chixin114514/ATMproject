01da11017fc1efee5c7afcce8e92f05823ecc3cd
XIE Boyang <bx4001@hw.ac.uk>
2024-11-15 10:13:37 +0800

    FinalVersion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import MainSrc.ATM;
-import MainSrc.Account;
 
 public class Main {
     public static void main(String[] args) {
--- src/MainSrc/ATM.java
+++ src/MainSrc/ATM.java
@@ -46,7 +46,7 @@ public class ATM {
 
     //主程序
     public void start(){
-        spawnTestAccount();
+       // spawnTestAccount();
         System.out.println("==ATM系统==");
         System.out.println("0、退出系统");
         System.out.println("1、用户登录");
@@ -147,7 +147,11 @@ public class ATM {
 
     }
     private void operatingSystem(Account account) {
+
         while (true) {
+            System.out.println("按回车进入下一步或确定");
+            scanner.nextLine();
+            scanner.nextLine();
             System.out.println("==========================");
             System.out.println("您已进入操作系统，可以选择办理以下业务");
             System.out.println("1、查询账户");
@@ -157,26 +161,32 @@ public class ATM {
             System.out.println("5、修改密码");
             System.out.println("6、退出");
             System.out.println("7、注销账户");
-            switch (scanner.nextInt()) {
-                case 1:
+            switch (scanner.next()) {
+                case "1":
                     checkAccount(account);
                     break;
-                    case 2:
+                    case "2":
                         Deposit(account);
+
                         break;
-                        case 3:
+                        case "3":
                             Withdraw(account);
+
                             break;
-                            case 4:
+                            case "4":
                                 Transfer(account);
+
                                 break;
-                                case 5:
+                                case "5":
                                     ChangePassword(account);
+
                                     break;
-                                    case 6:
+                                    case "6":
                                         return;
-                                        case 7:
-                                            Logout(account);
+                                        case "7":
+                                            deleteAccount(account);
+                                            return;
+
                                             default:
                                                 System.err.println("无效操作");
                                                 break;
@@ -195,16 +205,97 @@ public class ATM {
         System.out.println("账户余额："+account.getBalance());
         System.out.println("账户提现额度："+account.getWithdrawLimit());
         System.out.println("=====查询结束=====");
-        scanner.nextLine();
     }
     private void Deposit(Account account){
         System.out.println("请输入存入金额");
         account.setBalance(account.getBalance()+scanner.nextDouble());
+        System.out.println("存款成功,目前余额"+account.getBalance()+"元");
     }
-    private void Withdraw(Account account){}
-    private void Transfer(Account account){}
-    private void ChangePassword(Account account){}
-    private void Logout(Account account){}
+    private void Withdraw(Account account){
+        while (true) {
+            System.out.println("请输入取款金额（至少100元）");
+            double withdrawNumber = scanner.nextDouble();
+            if (withdrawNumber<100){
+                System.err.println("取款金额最低为100元");}
+            else {
+                if (withdrawNumber>account.getWithdrawLimit()){
+                    System.err.println("超过取款限额");
+                }
+                else {
+                    if(withdrawNumber>account.getBalance()){
+                        System.err.println("余额不足");
+                    }
+                    else{
+                    account.setBalance(account.getBalance()-withdrawNumber);
+                    System.out.println("成功取出"+withdrawNumber+"元,目前余额为"+account.getBalance()+"元");
+                    break;
+                    }}
+            }
+        }
+    }
+    private void Transfer(Account account){
+        System.out.println("请输入目标账户用户名");
+        String targetUserName;
+        double TransferAmount;
+        while (true) {
+            targetUserName = scanner.next();
+            if(!existUserName(targetUserName)){
+                System.err.println("未查询到此账户");
+                System.out.println("请重新输入账户");
+            }
+            else {break;}
+        }
+        while (true) {
+            System.out.println("请输入转账金额");
+             TransferAmount=scanner.nextDouble();
+            if (TransferAmount>account.getBalance()){
+                System.err.println("余额不足");
+            }
+            else {
+                if (TransferAmount>account.getWithdrawLimit()){
+                    System.err.println("转账金额超出限额");
+                }
+                else {break;}
+            }
+        }
+        getObejctbyUsername(targetUserName).setBalance(getObejctbyUsername(targetUserName).getBalance()+TransferAmount);
+        account.setBalance(account.getBalance()-TransferAmount);
+        System.out.println("成功给卡号"+getObejctbyUsername(targetUserName).getCardID()+"转账"+TransferAmount+"元。");
+
+    }
+    private void ChangePassword(Account account){
+        System.out.println("请输入当前密码");
+        String inputPassword = scanner.next();
+        if (inputPassword.equals(account.getPassword())){
+            System.out.println("请输入更改的密码");
+            String newPassword = scanner.next();
+            if (newPassword.equals(account.getPassword())){
+                System.err.println("两次密码重合");
+                return;
+            }
+            System.out.println("请再次输入更改的密码");
+            String secondPassword = scanner.next();
+            if (newPassword.equals(secondPassword)){
+                account.setPassword(newPassword);
+                System.out.println("更改成功");
+            }
+            else{
+                System.err.println("两次密码不一致");
+            }
+        }
+        else {
+            System.err.println("当前密码输入错误");
+        }
+    }
+    private void deleteAccount(Account account){
+        System.out.println("请输入当前账户密码");
+        if (scanner.next().equals(account.getPassword())){
+            System.err.println("您即将删除您的账户，请输入“我确定删除该账户”");
+            if (scanner.next().equals("我确定删除该账户")){
+                System.err.println("成功删除"+accountList.remove(account));
+            }
+        }
+    }
     }
 
 
